ceca39d12bbfd425036ccaf922a44195f15d5843
rhanafi26@gmail.com <rizahanafi98@gmail.com>
2026-08-21 10:29:07 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -138,8 +138,8 @@ io.on('connection', (socket) => {
 
 // ============ START SERVER ============
 const PORT = process.env.PORT || 5001;
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
   console.log(`🔌 Socket.io ready`);
 });
 
